288b59abeb0131988cfe23f07e3e5c9280d300f6
yuanquan <yuanquan2011@qq.com>
2019-05-07 15:43:09 +0800

    add excel reading class

--- MenuToExcel/src/excelReading.py
+++ MenuToExcel/src/excelReading.py
@@ -1,19 +1,42 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-import xlrd
+""" excel reading class based on openpyxl """
+import openpyxl
 import os, sys
 
-menuProcessfileName = 'L18-Y920-menuProgramProcess.xlsx'
-dataSheetName = u"五谷浆"
-os.chdir(sys.path[0])
-excelAbsPath = os.path.join(os.path.abspath('..'), 'document', menuProcessfileName)
+class excelRead():
+	""" class deal with excel reading based on openpyxl """
+	def __init__(self, fileName=None):
+		self.fileName = fileName
+		self.readOnly = True
+		self.curSheet = None
+
+	def fileGetAndLoad(self):
+		""" get and load the last excel in relative path. """
+		relativePath = "/./"
+		dirLists = os.listdir(os.getcwd()+relativePath)
+		for dirList in dirLists:
+			if dirList.split('.')[-1] == 'xlsx': self.fileName = dirList
+		if self.fileName == None:  exit("There is no excel file, please check your path!")
+		self.wb = openpyxl.load_workbook(self.fileName, self.readOnly)
+		print("open excel {} successfully.".format(self.fileName))
+		del dirLists
+
+	def sheetDatRead(self):
+		""" read all data from current sheet. """
+		self.curSheetDat = []
+		self.curRowDat = []
+		for row in self.curSheet.rows:
+			for cell in row:
+				self.curRowDat.append(cell.value)
+			self.curSheetDat.append(self.curRowDat)
+			self.curRowDat = []
 
 # read row data from excel by default.
 # data from float to secoud: second = float*240*60*60(1 day for 1)
-def excelDataRead(count=0, rowOrCol='row'):
-    data = xlrd.open_workbook(excelAbsPath)
-    table = data.sheet_by_name(dataSheetName)
-    if rowOrCol=='col': return table.col_values(count)
-    else: return table.row_values(count)
-
-print(excelDataRead(6)[2]*24*60*60)
+if __name__ == "__main__":
+	excelReading = excelRead()
+	excelReading.fileGetAndLoad()
+	excelReading.curSheet = excelReading.wb[excelReading.wb.sheetnames[0]]
+	excelReading.sheetDatRead()
+	print(excelReading.curSheetDat)
